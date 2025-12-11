23073b1939b8dbbce4c32109a547913a1f3d0ea6
vanhieun702-sketch <vanhieun702@gmail.com>
2025-12-11 08:42:59 +0700

    Delete 2.9 py.py

--- 2.9 py.py
+++
@@ -1,6 +0,0 @@
-from collections import Counter
-
-str_input = input("Enter a String:")
-count_dict = Counter(str_input)
-print(count_dict)
-# Lưu ý: Kết quả in ra sẽ là một đối tượng Counter, nhưng nó hoạt động như một từ điển.
